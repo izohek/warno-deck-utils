9923d9481d47e2f44284557a0c70c4907a0b06bb
izohek <izohek@gmail.com>
2023-02-07 13:55:55 -0800

    Added new test for war-yes encoding issue

--- tests/DeckEncoder.test.ts
+++ tests/DeckEncoder.test.ts
@@ -1,6 +1,8 @@
 import { decodeDeckString } from '../src/DeckStringDecoder'
 import { encodeDeck } from '../src/DeckStringEncoder'
 import { decks } from './TestData'
+import { Deck } from '../src/Deck';
+import { AllDivisions } from '@izohek/warno-db';
 
 test('test-encoding-decks', () => {
     decks.forEach(function (deck) {
@@ -10,4 +12,16 @@ test('test-encoding-decks', () => {
 
         expect(deckStringFromDecodedDeck).toBe(deck.deckString)
     })
+})
+
+test('failing-waryes-code', () => {
+
+    const  deck = new Deck();
+    deck.division = AllDivisions.find((div) => { return div.id === 20 })
+    deck.addUnitWithId(176, 1, null);
+
+    const deckString = encodeDeck(deck);
+
+    expect(deckString).toBe('FBFoGFcWAAAg')
+
 })
