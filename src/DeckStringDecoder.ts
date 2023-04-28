fc1ee855a14d17864051ed7ba66ff51aa0060971
izohek <izohek@gmail.com>
2023-04-28 11:56:17 -0700

    feat: allow lookup service to be optional.  parser will only return ids if no lookup service provided.

--- src/DeckStringDecoder.ts
+++ src/DeckStringDecoder.ts
@@ -8,7 +8,7 @@ import { SimpleDeck } from './types/SimpleDeck'
  * @param deckString
  * @returns
  */
-export function decodeDeckString (deckString: string, lookup: LookupService): SimpleDeck {
+export function decodeDeckString (deckString: string, lookup?: LookupService): SimpleDeck {
     // Parse and decode
     const parserResults = parseDeckString(deckString)
 
@@ -21,7 +21,7 @@ export function decodeDeckString (deckString: string, lookup: LookupService): Si
  * @param results DeckParser parse() results
  * @returns
  */
-export function deckFromParser (results: DeckParserResults, lookup: LookupService): SimpleDeck {
+export function deckFromParser (results: DeckParserResults, lookup?: LookupService): SimpleDeck {
     // Indicates if deck code was generated from a modded game
     const moddedFlag = parseInt(results.headers.modded.data as string ?? '', 2)
     const modded = moddedFlag === 1
@@ -33,7 +33,7 @@ export function deckFromParser (results: DeckParserResults, lookup: LookupServic
 
     const division = {
         id: divisionValue,
-        descriptor: lookup.divisionForId(divisionValue) ?? undefined
+        descriptor: lookup?.divisionForId(divisionValue)
     }
 
     const numberOfCardsField = results.headers.numberOfCards
@@ -44,12 +44,12 @@ export function deckFromParser (results: DeckParserResults, lookup: LookupServic
             veterancy: cardResult.xp,
             unit: {
                 id: cardResult.id,
-                descriptor: lookup.unitForId(cardResult.id)
+                descriptor: lookup?.unitForId(cardResult.id)
             },
             transport: cardResult.transport > 0
                 ? {
                     id: cardResult.transport,
-                    descriptor: lookup.unitForId(cardResult.transport)
+                    descriptor: lookup?.unitForId(cardResult.transport)
                 }
                 : undefined
         }
--- tests/DeckDecoder.test.ts
+++ tests/DeckDecoder.test.ts
@@ -70,3 +70,56 @@ describe('deck decoder tests', () => {
         expect(decodedDeck).toBeTruthy()
     })
 })
+
+// test decoding without an adapter
+describe('no adapter tests', () => {
+    decks.forEach( (deck) => {
+        test('test-first-deck-code-no-adapter', () => {
+            const deckCode = deck.deckString
+            const decodedDeck = decodeDeckString(deckCode)
+
+            // test division
+            expect(decodedDeck.division.id).toBe(deck.division)
+
+            // test expected number of cards
+            expect(decodedDeck.numberCards).toBe(deck.units.length)
+
+            // test expected number of cards matches actual # of cards
+            expect(decodedDeck.cards.length).toBe(decodedDeck.numberCards)
+
+            // test unit codes
+            const units = deck.units.map(function (u) {
+                if (Array.isArray(u)) {
+                    return u[0]
+                } else {
+                    return u
+                }
+            })
+
+            expect(decodedDeck.cards.map(function (c) {
+                return c.unit.id
+            }))
+                .toEqual(units)
+
+            // test transports
+            // transform the test data.  Stored as [unit] or [unit, transport].
+            const transports = deck.units.map(function (u) {
+                if (Array.isArray(u)) {
+                    return u[1]
+                } else {
+                    return null
+                }
+            })
+
+            const foundTransports = decodedDeck.cards.map((c) => { return c.transport?.id ?? null })
+            expect(
+                foundTransports
+            ).toEqual(transports)
+
+            // test veterancy
+            expect(
+                decodedDeck.cards.map((c) => { return c.veterancy })
+            ).toEqual(deck.veterancy)
+        })
+    })
+})
